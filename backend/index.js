74811f684d9c2305414bad24b7c414aba4c79add
hrithik <gajbhiyehrithik@gmail.com>
2025-12-17 16:06:05 +0530

    middleware to handle CORS

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,24 @@
 import express from "express"
+import cors from "cors"
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express()
+
+// middleware to handle cors
+
+app.use(
+  cors({
+    origin:process.env.FRONT_END_URL ||  "http://localhost:5173",
+    methods:["GET" , "POST" , "PUT" , "DELETE"],
+    allowedHeaders:["Content-Type","Authorization"],
+  })
+)
+
+// middleware to handle JSON object in req body
+app.use(express.json())
+
 
 app.listen(3000,()=>{
   console.log("server is running on port 3000!");
