b4b23f1d8449562c4e8ac8960229732341af64be
hrithik <gajbhiyehrithik@gmail.com>
2025-12-19 18:49:05 +0530

    get task by id api route

--- backend/controller/task.controller.js
+++ backend/controller/task.controller.js
@@ -108,7 +108,20 @@ export const getTasks = async (req,res,next)=>{
 } 
 
 
+export const getTasksById = async (req,res,next)=>{
+    try {
+        const task = await Task.findById(req.params.id).populate("assignedTo","name email profileImageUrl")
 
+        if(!task){
+            return next(errorHandler(404,"task not found"))
+        }
+
+        res.status(200).json(task)
+        
+    } catch (error) {
+        next(error)
+    }
+}
 
 
 
--- backend/routes/task.route.js
+++ backend/routes/task.route.js
@@ -1,10 +1,11 @@
 import express from "express"
 import { adminOnly, verifyToken } from "../utils/verifyUser.js"
-import { createTask, getTasks } from "../controller/task.controller.js"
+import { createTask, getTasks, getTasksById } from "../controller/task.controller.js"
 
 const router = express.Router()
 
 router.post("/create",verifyToken,adminOnly,createTask)
 router.get("/",verifyToken,getTasks)
+router.get("/:id",verifyToken,getTasksById)
 
 export default router
