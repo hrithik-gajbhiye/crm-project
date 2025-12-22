a599c6a62cf9fbfe1d88d7e08251bb1b788dda9f
hrithik <gajbhiyehrithik@gmail.com>
2025-12-22 17:23:33 +0530

    update task status api route

--- backend/routes/task.route.js
+++ backend/routes/task.route.js
@@ -1,6 +1,6 @@
 import express from "express"
 import { adminOnly, verifyToken } from "../utils/verifyUser.js"
-import { createTask, deleteTask, getTasks, getTasksById, updateTask } from "../controller/task.controller.js"
+import { createTask, deleteTask, getTasks, getTasksById, updateTask, updateTaskStatus } from "../controller/task.controller.js"
 
 const router = express.Router()
 
@@ -9,6 +9,7 @@ router.get("/",verifyToken,getTasks)
 router.get("/:id",verifyToken,getTasksById)
 router.put("/:id",verifyToken,updateTask)
 router.delete("/:id",verifyToken,adminOnly,deleteTask)
+router.put("/:id/status",verifyToken,updateTaskStatus)
 
 
 export default router
